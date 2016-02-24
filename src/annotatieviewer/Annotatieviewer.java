b7da402330a388a1a1daa23a27a1250373e396b3
Tjeerd <Tjeerd@145.74.26.53>
2016-02-24 14:09:20 +0100

    bye

--- src/annotatieviewer/Annotatieviewer.java
+++ src/annotatieviewer/Annotatieviewer.java
@@ -17,6 +17,7 @@ public class Annotatieviewer {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hoi");
+        System.out.println("doei");
     }
     
 }
