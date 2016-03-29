4455f2f9dcd15c8552e583b8fe9f8245cedc9fa1
emilvanamerongen <emilvanamerongen@gmail.com>
2016-03-29 17:12:24 +0200

    database connectie class toegevoegd met testquery (nog niet geimplementeerd) filereader annotatie inlezen toegevoegd (nog niet geimplementeerd)

--- src/annotatieviewer/File_reader.java
+++ src/annotatieviewer/File_reader.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import javax.swing.JFileChooser;
 
 /**
@@ -22,6 +23,10 @@ public class File_reader {
        if (type == 1){           
            readsequence(importfile);
        }
+       if (type == 2){
+           readannotation(importfile);
+          
+       }
 }    
     public static File openFileChooser(){
     String filename = File.separator+"tmp";
@@ -32,6 +37,7 @@ public class File_reader {
     }
     
     public static void readsequence(File importfile) throws IOException{
+        System.out.println("trying to read sequence file....");
         Path filepath = Paths.get(importfile.getAbsolutePath());
         
         String newsequence = "";
@@ -44,7 +50,37 @@ public class File_reader {
                         
             }
         }
-        System.out.println(newsequence);
+        System.out.println("new sequence loaded....");
+        Data_opslag_en_verwerking.setsequence(newsequence);
     }
     
+    public static void readannotation(File importfile) throws IOException{
+        System.out.println("trying to read gff annotation file....");
+        Path filepath = Paths.get(importfile.getAbsolutePath());
+        Boolean active = false;
+        int genenumber = 0;
+        int featurenumber = 0;
+        for (String line : Files.readAllLines(filepath)){
+            if (active){
+                String[] splitline = line.split("\t");
+                int feature_id = featurenumber;
+                featurenumber += 1;
+                
+                        
+            }
+            if (line.contains("\tgene\t")){
+                active = true;
+                String[] splitline = line.split("\t");
+                int geneid = genenumber;
+                genenumber += 1;
+                String discription = splitline[0];
+                int score = Integer.parseInt(splitline[5]);
+                HashMap<Integer, Feature> features = new HashMap<>();
+            }
+            if (line.contains("stop")){
+                active = false;
+            }
+        }
+        System.out.println("new sequence loaded....");
+    }
 }
