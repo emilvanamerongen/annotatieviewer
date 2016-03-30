5f15650a1611a67ea7c37a5ccc4b52e25bbc6eac
emilvanamerongen <emilvanamerongen@gmail.com>
2016-03-30 02:30:36 +0200

    begin gemaakt van visualisatie

--- src/annotatieviewer/Data_opslag_en_verwerking.java
+++ src/annotatieviewer/Data_opslag_en_verwerking.java
@@ -14,8 +14,8 @@ import java.util.HashMap;
 public class Data_opslag_en_verwerking {
 
     
-    public static String sequence;
-    public static HashMap<Integer,Gene> genes;
+    public static String sequence = "";
+    public static HashMap<Integer,Gene> genes = new HashMap<>();
     /**
      * @param args the command line arguments
      */
@@ -27,9 +27,11 @@ public class Data_opslag_en_verwerking {
     }
     public static void setsequence(String newsequence){
         sequence = newsequence;
+        Annotation_viewer_GUI.visualise();
     }
     public static void setgenes(HashMap<Integer,Gene> newgenes){
         genes = newgenes;
+        Annotation_viewer_GUI.visualise();
     }
     
 }
