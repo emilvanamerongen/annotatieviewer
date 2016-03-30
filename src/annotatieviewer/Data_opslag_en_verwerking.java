df598fb22244dac136984820919daccfab133a77
Tjeerd <Tjeerd@145.74.24.208>
2016-03-30 14:15:06 +0200

    bye

--- src/annotatieviewer/Data_opslag_en_verwerking.java
+++ src/annotatieviewer/Data_opslag_en_verwerking.java
@@ -5,6 +5,7 @@
  */
 package annotatieviewer;
 
+
 import java.util.HashMap;
 
 /**
@@ -16,8 +17,11 @@ public class Data_opslag_en_verwerking {
     
     public static String sequence = "";
     public static HashMap<Integer,Gene> genes = new HashMap<>();
+    public static HashMap<String,String> Translator = new HashMap<>(); //hij wou ze niet achter elkaar, dat gaf een foutmelding
     public static String AmminoSequence;
+    
     /**
+     * initiates the project
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -35,11 +39,16 @@ public class Data_opslag_en_verwerking {
      */
     public static void setsequence(String newsequence){
         sequence = newsequence;
+        AmminoSequence=TranslateSequence(sequence);
         Annotation_viewer_GUI.visualise();
     }
     public static String TranslateSequence(String sequence){
         String NewSequence="";
-        
+        int Nucleotides =sequence.length()/3;
+        for(int i=0;i<Nucleotides;i++){
+            String codon = (String) sequence.subSequence(i, i+3);
+            
+            }
         return NewSequence;
     }
     /**
@@ -49,6 +58,5 @@ public class Data_opslag_en_verwerking {
     public static void setgenes(HashMap<Integer,Gene> newgenes){
         genes = newgenes;
         Annotation_viewer_GUI.visualise();
-    }
-    
+    }    
 }
